ce7752323cbfa6be84e6ac0cff073a0ad1d11e32
Martin Omacht <enkeymc@gmail.com>
2019-03-04 14:14:14 +0100

    Parse methods return vector of ELFIssues #3

--- libelf/include/ELFReader.h
+++ libelf/include/ELFReader.h
@@ -6,7 +6,11 @@
 #define PROJECTELF_ELFREADER_H
 
 #include <istream>
+#include <vector>
 #include "ELF.h"
+#include "ELFIssue.h"
+
+using std::vector;
 
 namespace elf {
 
@@ -14,15 +18,15 @@ class ELFReader {
 public:
     explicit ELFReader(std::istream &istream, ELF &output);
 
-    void parse_header();
+    vector<ELFIssue> parse_header();
 
-    void parse_section_headers();
+    vector<ELFIssue> parse_section_headers();
 
-    void parse_program_headers();
+    vector<ELFIssue> parse_program_headers();
 
-    void parse_sections();
+    vector<ELFIssue> parse_sections();
 
-    void parse_segments();
+    vector<ELFIssue> parse_segments();
 
 private:
     std::istream &istream;
--- libelf/src/ELFReader.cpp
+++ libelf/src/ELFReader.cpp
@@ -7,3 +7,23 @@
 elf::ELFReader::ELFReader(std::istream &istream, elf::ELF &output) : istream(istream), elf(output) {
 
 }
+
+vector<ELFIssue> elf::ELFReader::parse_header() {
+    return vector<ELFIssue>();
+}
+
+vector<ELFIssue> elf::ELFReader::parse_section_headers() {
+    return vector<ELFIssue>();
+}
+
+vector<ELFIssue> elf::ELFReader::parse_program_headers() {
+    return vector<ELFIssue>();
+}
+
+vector<ELFIssue> elf::ELFReader::parse_sections() {
+    return vector<ELFIssue>();
+}
+
+vector<ELFIssue> elf::ELFReader::parse_segments() {
+    return vector<ELFIssue>();
+}
