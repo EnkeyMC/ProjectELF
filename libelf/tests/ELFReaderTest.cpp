7c4ac093b559145a81eeaa03e695b8b5e502ac27
Martin Omacht <enkeymc@gmail.com>
2019-03-28 23:38:34 +0100

    Moved ELFReaderTest to correct project, 64bit test

--- libelf/tests/ELFReaderTest.cpp
+++ libelf/tests/ELFReaderTest.cpp
@@ -109,3 +109,54 @@ TEST(ELFReaderTestNoFixture, parse_header_valid_32bit_msb_from_file) {
     EXPECT_EQ(elf.get_header().get_e_shnum(), 0);
     EXPECT_EQ(elf.get_header().get_e_shstrndx(), 0);
 }
+
+TEST_F(ELFReaderTest, parse_header_valid_64bit_msb) {
+    const char input[] = "\x7f\x45LF" // ei_mag
+            "\x02" // ei_class
+            "\x02" // ei_data
+            "\x01" // ei_version
+            "\x00" // ei_osabi
+            "\x00" // ei_abiversion
+            "\0\0\0\0\0\0\0" // padding bytes
+            "\x00\x02" // e_type (EXEC)
+            "\x00\x07" // e_machine
+            "\x00\x00\x00\x01" // e_version
+            "\x00\x00\x00\x00\x00\x00\xff\xff" // e_entry
+            "\x00\x00\x00\x00\x00\x00\x00\x00" // e_phoff
+            "\x00\x00\x00\x00\x00\x00\x00\x00" // e_shoff
+            "\x00\x00\x00\x00" // e_flags
+            "\x00\x40" // e_ehsize
+            "\x00\x00" // e_phentsize
+            "\x00\x00" // e_phnum
+            "\x00\x00" // e_shentsize
+            "\x00\x00" // e_shnum
+            "\x00\x00"; // e_shstrndx
+
+    setInput(input, sizeof(input));
+
+    auto issues = reader->parse_header();
+
+    EXPECT_TRUE(issues.empty()) << "Header parsing shouldn't return any issues";
+
+    EXPECT_EQ(elf->get_ei_mag0(), ELFMAG0);
+    EXPECT_EQ(elf->get_ei_mag1(), ELFMAG1);
+    EXPECT_EQ(elf->get_ei_mag2(), ELFMAG2);
+    EXPECT_EQ(elf->get_ei_mag3(), ELFMAG3);
+    EXPECT_EQ(elf->get_ei_class(), ELFCLASS64);
+    EXPECT_EQ(elf->get_ei_data(), ELFDATA2MSB);
+
+    EXPECT_EQ(elf->get_header().get_e_type(), ET_EXEC);
+    EXPECT_EQ(elf->get_header().get_e_machine(), EM_860);
+    EXPECT_EQ(elf->get_header().get_e_version(), EV_CURRENT);
+    EXPECT_EQ(elf->get_header().get_e_entry(), 0xFFFF);
+    EXPECT_EQ(elf->get_header().get_e_phoff(), 0);
+    EXPECT_EQ(elf->get_header().get_e_shoff(), 0);
+    EXPECT_EQ(elf->get_header().get_e_flags(), 0);
+    EXPECT_EQ(elf->get_header().get_e_ehsize(), 0x40);
+    EXPECT_EQ(elf->get_header().get_e_phentsize(), 0);
+    EXPECT_EQ(elf->get_header().get_e_phnum(), 0);
+    EXPECT_EQ(elf->get_header().get_e_shentsize(), 0);
+    EXPECT_EQ(elf->get_header().get_e_shnum(), 0);
+    EXPECT_EQ(elf->get_header().get_e_shstrndx(), 0);
+}
+
